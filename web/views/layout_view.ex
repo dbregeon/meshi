a382938ab320e4516ece5466546407c9e26a7fcf
dbregeon <denis.bregeon@digitalbrikes.com>
2017-10-17 23:10:00 +0900

    Fix env variable name.

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,5 +1,5 @@
 defmodule Meshi.LayoutView do
   use Meshi.Web, :view
 
-  def socket_url, do: System.get_env("WEBSOCKECT_URL") || "ws://localhost:4000/socket/websocket"
+  def socket_url, do: System.get_env("WEBSOCKET_URL") || "ws://localhost:4000/socket/websocket"
 end
